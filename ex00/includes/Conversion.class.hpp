987fba3f50f5d5f27fb4c94c6702ebe919a353a7
Maeva ALBRAND <malbrand@e1r1p3.42.fr>
2022-07-16 15:29:37 +0200

    ex00

--- ex00/includes/Conversion.class.hpp
+++ ex00/includes/Conversion.class.hpp
@@ -6,7 +6,7 @@
 /*   By: malbrand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 02:26:49 by malbrand          #+#    #+#             */
-/*   Updated: 2022/07/16 03:35:35 by malbrand         ###   ########.fr       */
+/*   Updated: 2022/07/16 15:26:42 by malbrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,54 @@
 
 # include <iostream>
 # include <exception>
+# include <cstdlib>
+# include <limits>
 
 class Conversion
 {
 	private :
+			char				_char;
+			int					_int;
+			std::string			_type;
+			float				_float;
+			double				_double;	
 			const std::string	_target;
-			std::string	_type;
 	public :
+			Conversion(void);
 			Conversion(const std::string av);
+			Conversion(const Conversion &rhs);
 			~Conversion(void);
 
-			std::string	checkVar(void) const;
+			int					getInt(void)const;
 
+			char				getChar(void)const;
+
+			double				getDouble(void)const;
+
+			float				getFloat(void)const;
+
+			void				convert(void);
+			void				fromInt(void);
+			void				fromChar(void);
+			void				fromFloat(void);
+			void				fromDouble(void);
+			void				printChar(void)const;
+			void				printInt(void)const;
+			void				printFloat(void)const;
+			void				printDouble(void)const;
+
+			std::string			getType(void)const;
+			std::string			checkVar(void) const;
+
+			Conversion &operator=(const Conversion &rhs);
+	
 			class StringEmpty : public std::exception
 			{
 				const char *what() const throw() {return "Need not empty argument.";}
+			};
+			class BadType : public std::exception
+			{
+				const char *what() const throw() {return "Need char, int, float or double.";}
 			};
 };
 
